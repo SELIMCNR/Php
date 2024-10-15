b20892e3ad4dbfb7d07d6a1f22d732dd2f0b7b90
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-10-15 22:53:24 +0300

    Add files via upload

--- php8/php_19_reference_value.php
+++ php8/php_19_reference_value.php
@@ -3,6 +3,42 @@
   // reference
   // value
 
+  function toplama($sayi){
+    $sayi +=10;             // value tipte
+    echo $sayi."<br>";
+  }
+
+  $ornekSayi=5;
+  toplam($ornekSayi);  // fonksiyonda value değer gelir
+
+  echo $ornekSayi;  // globaldeki değişken değeri gelir
+
+  
+  function toplama1(&$sayi){
+    $sayi +=10;             // value tipte
+    echo $sayi."<br>";
+  }
+
+  $ornekSayi=5;
+  toplam($ornekSayi);  // fonksiyonda reference değer gelir
+
+  echo $ornekSayi;  // reference değeri gelir
+
+
+  function yasHesapla(&$tarihler){
+        for($i=0;$i<count($tarihler); $i++){
+          $tarihler[$i]=2023-$tarihler[$i];
+        }
+        return $tarihler;
+  }
+
+  $liste = array(1990,1985,2000);
+
+  echo "<pre>";
+  print_r(yasHesapla($liste));
+  print_r($liste);
+  echo "</pre>";
+
   
 
     
---
+++ php8/php_20_tanimsizparametre.php
@@ -0,0 +1,17 @@
+<?php 
+
+      function intro(){
+        $parametreSayisi = func_num_args();
+        if($parametreSayisi ==0){
+          echo "parametre yok";
+        }
+        else {
+          $parametreler = func_get_args();
+
+          foreach($parametreler as $parametre){
+            echo $parametre."<br>";
+          }
+        }
+      }
+
+?>
