5a488ca8fe3247cfbecfb209f277532e5935255d
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-12-06 18:14:01 +0300

    Add files via upload

--- php8/bölüm-9/upload.php
+++ php8/bölüm-9/upload.php
@@ -1,54 +1,75 @@
 <?php 
 
-    if(isset($_POST["btnFileUpload"])  && $_POST["btnFileUpload"]=="Upload" ){
+        if(isset($_POST["btnFileUpload"])  && $_POST["btnFileUpload"]=="Upload" ){
 
-        /*
-        echo "<pre>";
-        
+            /*
+            echo "<pre>";
+            
 
-        print_r($_FILES["fileToUpload"]);
-        print_r($_POST);
-        echo"  </pre>";
-*/
+            print_r($_FILES["fileToUpload"]);
+            print_r($_POST);
+            echo"  </pre>";
+    */
+            if(isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"]==0) 
+                {
+                    $upload0k=true;
 
-        $upload0k=true;
-
-        $dest_path= "./uploadedFiles/";
-        $filename = $_FILES["fileToUpload"]["name"];
-        $fileSize = $_FILES["fileToUpload"]["size"];
-
-        if(empty($filename)){
-            $upload0k = false;
-            echo "dosya seçiniz";
-            echo "<br>";
-        }
-        
-        if($fileSize > 500000000){
-            echo "Dosya boyutu fazla";
-            $upload0k = false;
-            echo "<br>";
-        }
-
-        
-        
-        $fileSourcePath=$_FILES["fileToUpload"]["tmp_name"];
-
-        $fileDestPath=$dest_path.$filename;
-        
-        if(!$upload0k){
-            echo "dosya yüklenmedi";
-        }
-        else {
-            if(move_uploaded_file($fileSourcePath,$fileDestPath)){
-                echo "dosya yüklendi";
-            }
+                    $dest_path= "./images/";
+                    $filename = $_FILES["fileToUpload"]["name"];
+                    $fileSize = $_FILES["fileToUpload"]["size"];
+                    $dosya_uzantilari = array('jpg','jpeg','png');
+            
+                    if(empty($filename)){
+                        $upload0k = false;
+                        echo "dosya seçiniz";
+                        echo "<br>";
+                    }
+                    
+                    if($fileSize > 500000000){
+                        echo "Dosya boyutu fazla";
+                        $upload0k = false;
+                        echo "<br>";
+                    }
+            
+                    // 1.jpg => 1, jpg
+                    $dosyaAdi = explode(".",$filename); 
+                    $dosyaAdi_uzantisisiz = $dosyaAdi[0];
+                    $dosyaAdi_uzantisi = $dosyaAdi[1];
+            
+                    if(!in_array($dosyaAdi_uzantisi,$dosya_uzantilari)){
+                        $upload0k=false;
+                        echo "dosya uzantısı kabul edilmiyor";
+                        echo "kabul edilen dosyalar : ".implode(",",$dosya_uzantilari);
+                        echo "<br>";    
+                    }
+            
+                    $yeni_dosyaAdi= md5(time().$dosyaAdi_uzantisisiz).'.'.$dosyaAdi_uzantisi;
+            
+            
+                    
+                    $fileSourcePath=$_FILES["images"]["tmp_name"];
+            
+                    $fileDestPath=$dest_path.$yeni_dosyaAdi;
+                    
+                    if(!$upload0k){
+                        echo "dosya yüklenmedi";
+                    }
+                    else {
+                        if(move_uploaded_file($fileSourcePath,$fileDestPath)){
+                            echo "dosya yüklendi";
+                        }
+                        else {
+                            echo "hata";
+                        }
+                    }
+                }
             else {
-                echo "hata";
-            }
-        }
+                    echo "bir hata oluştu";
+                }
+            
+            
         
-       
-    }
+        }
 
 
 ?>
