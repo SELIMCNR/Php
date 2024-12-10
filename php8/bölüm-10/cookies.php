9fbaf6b7da3b8cf5094bc4d4441ee161100a4667
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-12-10 23:06:52 +0300

    Add files via upload

---
+++ php8/bölüm-10/cookies.php
@@ -0,0 +1,25 @@
+<?php
+
+    setcookie("username","selimcinar",time()+(60*60*24));
+
+    setcookie("auth","true",time()+(60*60*24));
+    
+
+if(isset($_COOKIE["username"])){
+    echo $_COOKIE["username"];
+
+}
+else {
+    echo "cookie yok";
+}
+
+echo $_COOKIE["auth"];
+
+// güncelleme
+setcookie("username","admin",time()+(60*60*24));
+    
+//silme
+setcookie("username","sadikturan",time()-(3600));
+    
+
+?>
